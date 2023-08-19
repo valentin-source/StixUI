36fae7d091caf77ec21f6eed0e4f8458b9d7a97d
vbsource <73877232+valentin-source@users.noreply.github.com>
2023-08-19 12:03:04 +0200

    Update prepare.sh
    prepare.sh now also installs npm if not present

--- prepare.sh
+++ prepare.sh
@@ -1,9 +1,39 @@
+#!/bin/bash
+
+# Check if npm is already installed
+if ! command -v npm &> /dev/null; then
+    echo "npm is not installed. Installing..."
+
+    # Check which package manager is available and install npm accordingly
+    if command -v apt &> /dev/null; then
+        sudo apt update
+        sudo apt install -y npm
+    elif command -v apt-get &> /dev/null; then
+        sudo apt-get update
+        sudo apt-get install -y npm
+    elif command -v dnf &> /dev/null; then
+        sudo dnf install -y npm
+    else
+        echo "Unsupported package manager. Please install npm manually."
+        exit 1
+    fi
+
+    echo "npm has been installed."
+else
+    echo "npm is already installed."
+fi
+
+echo "now installing Node Version Manager"
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 source ~/.bashrc
+echo "installing Node:11"
 nvm install 11
 nvm use 11
+echo "Pulling StixUI project"
 git pull https://github.com/valentin-source/StixUI.git
 cd ./StixUI/app
+echo "Building and running Project:"
 npm run build
 cd ./build
 xdg-open ./index.html
